ff41c691dd20bd55fa0349b52be8c4536068dcc1
nigma <spiritofenergy220gmail.com>
2022-09-24 14:12:26 +0300

    Добавил Api

--- app/src/main/java/com/kodexcompany/iskranavigation/AppBar.kt
+++ app/src/main/java/com/kodexcompany/iskranavigation/AppBar.kt
@@ -1,12 +1,13 @@
 package com.kodexcompany.iskranavigation
 
-import android.graphics.drawable.Icon
 import androidx.compose.material.*
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import org.w3c.dom.Text
+import androidx.wear.compose.material.Icon
+import androidx.wear.compose.material.MaterialTheme
 
 @Composable
 fun AppBar(
@@ -16,8 +17,8 @@ fun AppBar(
         title = {
             Text(text = stringResource(id = R.string.app_name))
         },
-        backgroundColor = MaterialTheme.colors.primary,
-        contentColor = MaterialTheme.colors.onPrimary,
+      //  backgroundColor = MaterialTheme.colors.primary,
+        contentColor = contentColorFor(backgroundColor),
         navigationIcon = {
             IconButton(onClick = onNavigationIconClick) {
                 Icon(
---
+++ app/src/main/java/com/kodexcompany/iskranavigation/screecs/SplashScreen.kt
@@ -0,0 +1,10 @@
+package com.kodexcompany.iskranavigation.screecs
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.wear.compose.material.Text
+
+@Composable
+fun SplashScreen (navController: NavController){
+    Text(text = "Это Splash")
+}
